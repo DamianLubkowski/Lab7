0805b48c0cc3bab4bf7e642900b82251db795b47
DamianLubkowski <multiclick@wp.pl>
2022-09-22 22:22:05 +0200

    Punkt 6, aktualizacja metody connect()

--- src/main/java/pl/lublin/wsei/java/cwiczenia/MyDB.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/MyDB.java
@@ -37,6 +37,7 @@ public class MyDB {
 
         try {
             conn = DriverManager.getConnection(jdbcString, connectionProps);
+            statement = conn.createStatement();
         }
         catch(SQLException e) {
             System.out.println("Błąd podłączenia do bazy: " + jdbcString);
